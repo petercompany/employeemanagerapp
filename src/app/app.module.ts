e323af16a5a9643f094da354a1cf7b744fcee233
petercompany <83719040+petercompany@users.noreply.github.com>
2021-05-12 16:39:16 -0500

    ok

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EmployeeService } from './employee.service';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule, FormsModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
